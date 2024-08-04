595e52d0e8e577d71e71491b696b3e080b5df882
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-08-05 00:38:17 +0700

    update: navbar looks more stylish

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -7,34 +7,36 @@ const montserrat = Montserrat({
 })
 export default function page() {
   return (
-    <main className={montserrat.className}>
+    <>
+        <section id='main' className={montserrat.className}>
 
-        <div className='w-full h-[100vh] opacity-100'>
-            <Image
-            src={'/pexels-mareefe-672051.jpg'}
-            alt='Freepik Image - Mountain'
-            width={0}
-            height={0}
-            sizes='100%'
-            style={{width: '100%', height: '100vh', objectFit: 'cover'}}
-            className='absolute'
-            priority
-            />
+            <div className='w-full h-[100vh] opacity-100'>
+                <Image
+                src={'/pexels-mareefe-672051.jpg'}
+                alt='Freepik Image - Mountain'
+                width={0}
+                height={0}
+                sizes='100%'
+                style={{width: '100%', height: '100vh', objectFit: 'cover'}}
+                priority
+                />
 
-            <div className='absolute grid items-center top-0 left-0 w-full h-full'>
-                <div className='rounded-lg bg-black bg-opacity-25 backdrop-blur-sm p-10 md:w-1/3'>
-                    <div className='md:text-3xl lg:text-5xl font-extrabold text-white' >
-                        The Most <span>Reliable Distributor</span> Aromatic Oils <span>In Indonesia</span>
+                <div className='absolute grid items-center top-0 left-0 w-full h-full'>
+                    <div className='rounded-lg bg-black bg-opacity-25 backdrop-blur-sm p-10 md:w-1/3'>
+                        <div className='md:text-3xl lg:text-4xl font-extrabold text-white' >
+                            The Most <span className='text-transparent md:text-3xl lg:text-5xl bg-gradient-to-br from-gray-200 to-[#FFDE4D] bg-clip-text'>Reliable Distributor Aromatic Oils </span> In Indonesia
+                        </div>
                     </div>
                 </div>
+
+                <div></div>
             </div>
-        </div>
+        </section>
 
-        
-
-        <div className='h-[240px]'>
-            Hellow
-        </div>
-    </main>
+        <section>
+            <div>Hellow</div>
+        </section>
+    </>
+    
   )
 }
--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -5,12 +5,11 @@ import Link from 'next/link'
 import React, { useRef } from 'react'
 import LogoBrand from './logo/logoBrand'
 import Logo from './logo/logo'
-
-
+import { usePathname } from 'next/navigation'
 
 
 export default function Navbar() {
-
+    const pathName = usePathname();
     const navHamburger = useRef(null)
     const handleHamburger = (e: any) => {
         e.current.hidden = !e.current.hidden
@@ -18,33 +17,50 @@ export default function Navbar() {
     }
 
     const linkClassName = () => {
-        return 'font-semibold p-3 rounded-lg hover:bg-[#89926b] hover:shadow-md transition'
+        return `font-semibold py-2 lg:py-3 transition first:rounded-bl-full first:rounded-tl-full last:rounded-tr-full last:rounded-br-full
+        [&_p]:px-4 lg:[&_p]:px-6 [&_p]:border-r-2 [&_p]:border-gray-500 [&_p]:hover:underline [&_p]:hover:underline-offset-4 [&_p]:hover:decoration-2 [&_p]:hover:decoration-black
+        [&_p]:first:pl-6 lg:[&_p]:first:pl-8 [&_p]:last:pr-6 lg:[&_p]:last:pr-8 [&_p]:last:border-r-0`
     }
-    
+
+    const bgBlack = () => {
+        return `bg-black`
+    }
+
+    const handleActiveLink = (pathRef: string) => {
+        return pathRef === pathName? 'underline underline-offset-4 decoration-2 pointer-events-none' : ''
+    }   
   return (
-    <nav className='fixed top-0 w-full h-auto p-5 z-50  bg-[rgb(181,193,142)]/70 shadow-md backdrop-blur-sm'>
+    <nav className='md:text-lg fixed top-0 w-full h-auto p-5 z-50  bg-[rgb(181,193,142)]/50 shadow-md backdrop-blur-sm'>
         
         <div className='hidden md:flex justify-around items-center'>
-            <Link href={'/'} className='flex gap-4'>
+            <Link href={'/'} className='flex gap-4 items-center'>
                 <Logo />
                 <LogoBrand />
             </Link>
-            <div >
-                <Link className={linkClassName()} href={'/'}>Home</Link>
-                <Link className={linkClassName()} href={'/about'}>About Us</Link>
-                <Link className={linkClassName()} href={'/products'}>Products</Link>
-                <Link className={linkClassName()} href={'/team'}>Team</Link>
+            <div className='flex rounded-full divide-gray-400 bg-gray-100/50'>
+                <Link className={`${linkClassName()} ${handleActiveLink('/')}`} href={'/'}>
+                    <p>Home</p>
+                </Link>
+                <Link className={`${linkClassName()} ${handleActiveLink('/about')}`} href={'/about'}>
+                    <p>About Us</p>
+                </Link>
+                <Link className={`${linkClassName()} ${handleActiveLink('/products')}`} href={'/products'}>
+                    <p>Products</p>
+                </Link>
+                <Link className={`${linkClassName()} ${handleActiveLink('/team')}`} href={'/team'}>
+                    <p>Team</p>
+                </Link>
             </div>
             
-            <button className='p-3 bg-white rounded-lg shadow-md hover:underline hover:decoration-2 hover:shadow-lg font-semibold transform- hover:w-1/2'>
+            <Link href={'/contact'} className='p-3 bg-white pr- rounded-lg shadow-md hover:underline hover:underline-offset-4 hover:decoration-2 hover:shadow-lg font-semibold hover:bg-[#EADBC8]'>
                 Join Us Now
-            </button>
+            </Link >
         </div>
             
         <div className='md:hidden flex flex-col'>
             <div>
                 <div className='flex flex-row justify-between'>
-                    <Link href={'/'} className='flex mr-12 gap-4'>
+                    <Link href={'/'} className='flex mr-12 gap-4 items-center'>
                         <LogoBrand />
                         <Logo />
                     </Link>
@@ -66,9 +82,9 @@ export default function Navbar() {
                     <Link className={linkClassName()} href={'/about'}>About Us</Link>
                     <Link className={linkClassName()} href={'/products'}>Products</Link>
                     <Link className={linkClassName()} href={'/team'}>Team</Link>
-                    <button className='m-3 w-1/2 mx-auto bg-white rounded-lg shadow-md hover:shadow-lg font-semibold'>
+                    <Link href={'/contact'} className='m-3 w-1/2 mx-auto text-center bg-white rounded-lg shadow-md hover:shadow-lg font-semibold'>
                         Join Us Now
-                    </button>
+                    </Link>
                 </div>
             </div>
         </div>
