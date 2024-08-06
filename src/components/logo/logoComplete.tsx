550feefb819859fdfdd0dfe9fd0b50d85c559311
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-08-06 12:53:26 +0700

    add: Complete Logo DOM

---
+++ src/components/logo/logoComplete.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import LogoBrand from './logoBrand'
+import Logo from './logo'
+
+export default function LogoComplete() {
+  return (
+    <div className='flex items-center gap-4'>
+            <Logo />
+            <LogoBrand />
+    </div>
+  )
+}
