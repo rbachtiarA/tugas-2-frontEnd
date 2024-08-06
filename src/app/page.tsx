f3e81a51a878e0c96d2591914148a6a25e6f6c65
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-08-06 12:56:50 +0700

    add: content at hero section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 import Carousel from '@/components/carousel'
+import Hero from '@/components/hero'
 import { Montserrat } from 'next/font/google'
 import Image from 'next/image'
 import React from 'react'
@@ -9,33 +10,8 @@ const montserrat = Montserrat({
 export default function page() {
   return (
     <>
-        <section id='main' className={montserrat.className}>
-
-            <div className='w-full h-[100vh] opacity-100'>
-                <Image
-                src={'/pexels-mareefe-672051.jpg'}
-                alt='Freepik Image - Mountain'
-                width={0}
-                height={0}
-                sizes='100%'
-                style={{width: '100%', height: '100vh', objectFit: 'cover'}}
-                priority
-                />
-
-                <div className='absolute grid items-center top-0 left-0 w-full h-full'>
-                    <div className='rounded-lg bg-black bg-opacity-25 backdrop-blur-sm p-10 md:w-1/3'>
-                        <div className='md:text-3xl lg:text-4xl font-extrabold text-white' >
-                            The Most <span className='text-transparent md:text-3xl lg:text-5xl bg-gradient-to-br from-gray-200 to-[#FFDE4D] bg-clip-text'>Reliable Distributor Aromatic Oils </span> In Indonesia
-                        </div>
-                    </div>
-                </div>
-
-                <div>
-                    
-                </div>
-            </div>
-        </section>
-
+        <Hero />
+        
         <section>
             <div>Hellow</div>
         </section>
