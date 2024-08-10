cc4066d0ef7e13d7f7515bb393ee25ac35efa57e
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-08-10 23:12:06 +0700

    edit: give useRef html element type

--- src/components/youtubeEmbed.tsx
+++ src/components/youtubeEmbed.tsx
@@ -3,16 +3,18 @@ import Image from "next/image"
 import { useRef } from "react"
 
 export default function YoutubeEmbed({ link, title, staticImg }: { link: string, title: string, staticImg:string }) {
-  const useBtn = useRef(null)
+  const useBtn = useRef<HTMLDivElement>(null)
   const handleOnClick = () => {
-    const node = `<iframe 
-        width="100%" height="215" 
-        src=${link} 
-        loading="lazy"
-        title="YouTube video player" 
-        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        />`
-    useBtn.current.innerHTML = node
+    if(useBtn.current !== null) {
+      const node = `<iframe 
+          width="100%" height="215" 
+          src=${link} 
+          loading="lazy"
+          title="YouTube video player" 
+          allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
+          />`
+      useBtn.current.innerHTML = node
+    }
   }  
   return (
     <div className='flex flex-col items-center justify-end w-[300px] md:w-[360px]'>
