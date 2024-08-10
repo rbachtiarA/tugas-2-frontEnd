3ea963e47febce39569cadd3a322db23469cff02
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-08-10 23:04:42 +0700

    add: change youtube embed to static image on click generate youtube embed

--- src/components/CompanyVideo.tsx
+++ src/components/CompanyVideo.tsx
@@ -11,10 +11,12 @@ export default function CompanyVideo() {
     <div className={`flex flex-wrap justify-center gap-8 p-4 bg-[#838b66] text-white ${tw.className}`}>
       <YoutubeEmbed 
       link="https://www.youtube.com/embed/lYm6pQU6bEE?si=ImdPK7UN4TKpRBQQ" 
-      title="How our company produce essential oil" />
+      title="How our company produce essential oil"
+      staticImg='/ss-produce-embed.png' />
       <YoutubeEmbed 
       link="https://www.youtube.com/embed/ORISJjsCEro?si=l_V6E83vUP7tT14Y" 
-      title="How to use our product properly" />
+      title="How to use our product properly"
+      staticImg='/ss-safety-embed.png' />
     </div>
   )
 }
--- src/components/youtubeEmbed.tsx
+++ src/components/youtubeEmbed.tsx
@@ -1,14 +1,35 @@
-export default function YoutubeEmbed({ link, title }: { link: string, title: string }) {
-  return (
-    <div className='flex flex-col items-center justify-end w-[300px] md:w-[360px]'>
-        <h1 className='font-bold mb-2'>{title}</h1>
-        <iframe 
+'use client'
+import Image from "next/image"
+import { useRef } from "react"
+
+export default function YoutubeEmbed({ link, title, staticImg }: { link: string, title: string, staticImg:string }) {
+  const useBtn = useRef(null)
+  const handleOnClick = () => {
+    const node = `<iframe 
         width="100%" height="215" 
-        src={link} 
+        src=${link} 
         loading="lazy"
         title="YouTube video player" 
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" 
-        />
+        />`
+    useBtn.current.innerHTML = node
+  }  
+  return (
+    <div className='flex flex-col items-center justify-end w-[300px] md:w-[360px]'>
+        <h1 className='font-bold mb-2'>{title}</h1>
+        <div ref={useBtn} className="h-[215px]">
+          <button onClick={handleOnClick}>
+            <Image
+            src={staticImg}
+            alt={title}
+            width={0}
+            height={0}
+            sizes="100vw"
+            style={{width: '100%', height: '100%'}}
+            />
+          </button>
+        </div>
+        
     </div>
   )
 }
