c76ed9e6baf1d080f9d13b37124cb990967c4c42
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-08-10 22:00:16 +0700

    edit: fix import false name

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,13 +1,9 @@
-import Carousel from '@/components/carousel'
-import CompanyTimeline from '@/components/companyTimeline'
 import CompanyVideo from '@/components/CompanyVideo'
-import Founder from '@/components/Founder'
+import Founder from '@/components/founder'
 import Hero from '@/components/hero'
 import Highlight from '@/components/highlight'
 import ProductPopular from '@/components/productPopular'
 import ReviewCard from '@/components/reviewCard'
-import { Montserrat } from 'next/font/google'
-import Image from 'next/image'
 import React from 'react'
 
 
