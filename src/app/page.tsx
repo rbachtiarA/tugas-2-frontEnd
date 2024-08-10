37b9db44a2e56fbd0afc420777ecd2e18989728e
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-08-10 22:38:55 +0700

    edit: without youtube embed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -20,7 +20,7 @@ export default function page() {
         <Highlight />
         <Suspense fallback={<Loading />}>
           <Founder />
-          <CompanyVideo />
+          {/* <CompanyVideo /> */}
         </Suspense>
         
         <ProductPopular />
