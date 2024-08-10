cb3dba8af9559c186ba80308b147241729c3e092
rbachtiarA <rbachtiarabdalla@gmail.com>
2024-08-10 21:56:22 +0700

    add: more detailed page

--- src/app/products/[slug]/page.tsx
+++ src/app/products/[slug]/page.tsx
@@ -1,4 +1,5 @@
 
+import convertMoney from '@/app/lib/moneyChanger'
 import { getProductDataBySlug } from '@/app/lib/product'
 import Image from 'next/image'
 import React from 'react'
@@ -15,18 +16,36 @@ export default async function page({ params }: {params: { slug: string}}) {
   return (
     <div className=''>
 
-      <div className='flex flex-col-reverse md:flex-row md:h-[400px]'>
-        <div className='md:w-1/2'>
-          <div className='font-semibold'>
-            {data.fields.name}
+          <div className='py-4 text-center text-2xl font-semibold'>
+            <h1>
+              {data.fields.name}
+            </h1>  
           </div>
+      <div className='flex flex-col-reverse md:flex-row md:h-full p-4 md:p-0'>
+        <div className='md:w-1/2 p-2 flex flex-col gap-4'>
 
           <div>
             {data.fields.description}
           </div>
 
           <div>
-            
+            <h2 className='font-semibold'>Suggested Used:</h2>
+            <ul className='list-disc list-inside *:my-2'>
+              {data.fields.suggest.map((suggest, idx) => (
+                <li key={idx}>{suggest.replaceAll(';',',')}</li>
+              ))}
+
+            </ul>
+          </div>
+
+          <div>
+            <h2>Price : {convertMoney(data.fields.price)}</h2>
+          </div>
+
+          <div>
+            <button className='bg-white hover:bg-black hover:text-white px-4 py-2 rounded-full transition'>
+              Add to Cart
+            </button>
           </div>
         </div>
 
